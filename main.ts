fe751c9e5a8cfaf9910bd9f59101c400d638fe3d
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-03-05 12:52:12 -0500

    Changed and Committed

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Yusuf Ahmed
+ * Created on: Mar 2026
+ * This program is a cookie clicker
 */
 
-basic.showString('Hello, World!')
+let cookieCount: number = 0
+
+basic.showIcon(IconNames.Happy)
+
+// cookie count goes up by 1 each time A is clicked
+input.onButtonPressed(Button.A, function () {   
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    cookieCount = cookieCount +1
+    basic.pause(100)
+    basic.showNumber(cookieCount)
+})
+
+// bring cookie count back to 0 when B is clicked
+input.onButtonPressed(Button.B, function () {
+    basic.pause(100)
+    basic.showIcon(IconNames.No)
+    cookieCount = 0
+    basic.showNumber(cookieCount)
+})
