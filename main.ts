cf24b4c8520c1d09701e2851900766aae3d5a984
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-03-05 13:26:59 -0500

    Changed and Committed

--- main.ts
+++ main.ts
@@ -13,16 +13,12 @@ basic.showIcon(IconNames.Happy)
 // cookie count goes up by 1 each time A is clicked
 input.onButtonPressed(Button.A, function () {   
     basic.clearScreen()
-    basic.pause(100)
     cookieCount = cookieCount +1
-    basic.pause(100)
     basic.showNumber(cookieCount)
 })
 
 // bring cookie count back to 0 when B is clicked
 input.onButtonPressed(Button.B, function () {
-    basic.pause(100)
     cookieCount = 0
-    basic.pause(100)
     basic.showNumber(cookieCount)
 })
