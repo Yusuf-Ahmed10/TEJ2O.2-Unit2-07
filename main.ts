1a69eaff62dddb5a190700bc613aea5f715eef50
Yusuf-Ahmed10 <yusuf.ahmed1@stu.ocsb.ca>
2026-03-05 13:25:25 -0500

    Changed and Committed

--- main.ts
+++ main.ts
@@ -2,17 +2,18 @@
  *
  * Created by: Yusuf Ahmed
  * Created on: Mar 2026
- * This program is a cookie clicker
+ * This program is a cookie clicker.
 */
 
 let cookieCount: number = 0
 
+// setup
 basic.showIcon(IconNames.Happy)
 
 // cookie count goes up by 1 each time A is clicked
 input.onButtonPressed(Button.A, function () {   
     basic.clearScreen()
-    basic.showIcon(IconNames.Yes)
+    basic.pause(100)
     cookieCount = cookieCount +1
     basic.pause(100)
     basic.showNumber(cookieCount)
@@ -21,7 +22,7 @@ input.onButtonPressed(Button.A, function () {
 // bring cookie count back to 0 when B is clicked
 input.onButtonPressed(Button.B, function () {
     basic.pause(100)
-    basic.showIcon(IconNames.No)
     cookieCount = 0
+    basic.pause(100)
     basic.showNumber(cookieCount)
 })
